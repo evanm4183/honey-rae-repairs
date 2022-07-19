791c5acc57e8a71296f3e392ce828075e3a3d158
Evan Mitchell <evanm4183@gmail.com>
2022-07-19 15:58:40 -0500

    end of day commit

--- src/components/tickets/Ticket.js
+++ src/components/tickets/Ticket.js
@@ -2,10 +2,51 @@ import { Link } from "react-router-dom"
 
 export const Ticket = ({ticket, employees, currentUser, getAllTickets}) => {
     let employee = null
+    const userEmployee = employees.find(employee => employee.userId === currentUser.id)
 
     if (ticket.employeeTickets.length > 0) {
         const employeeTicket = ticket.employeeTickets[0]
         employee = employees.find(employee => employee.id === employeeTicket.employeeId)
+    }
+
+    const canClose = () => {
+        if (userEmployee?.id === employee?.id && ticket.dateCompleted === "") {
+            return <button className="ticket__finish" onClick={closeTicket}>Finish</button>
+        } else {
+            return ""
+        }
+    }
+
+    const deleteButton = () => {
+        if (!currentUser.staff) {
+            return <button className="ticket__delete" onClick={() => {
+                fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, {
+                    method: "DELETE"
+                })
+                .then(() => {getAllTickets()})
+            }}>Delete</button>
+        } else {
+            return ""
+        }
+    }
+
+    const closeTicket = () => {
+        const copy = {
+            userId: ticket.userId,
+            description: ticket.description,
+            emergency: ticket.emergency,
+            dateCompleted: new Date()
+        }
+
+        return fetch(`http://localhost:8088/serviceTickets/${ticket.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(copy)
+            })
+            .then(() => {getAllTickets()})
+        
     }
 
     return <section className="ticket" key={ticket.id}>
@@ -31,7 +72,7 @@ export const Ticket = ({ticket, employees, currentUser, getAllTickets}) => {
                                     "Content-Type": "application/json"
                                 }, 
                                 body: JSON.stringify({
-                                    employeeId: employees.find(employee => employee.userId === currentUser.id).id, 
+                                    employeeId: userEmployee.id, 
                                     serviceTicketId: ticket.id})
                             })
                             .then(response => response.json())
@@ -40,6 +81,8 @@ export const Ticket = ({ticket, employees, currentUser, getAllTickets}) => {
                         >Claim</button>
                     : ""
         }
+        {canClose()}
+        {deleteButton()}
     </footer>
 </section>
 }
